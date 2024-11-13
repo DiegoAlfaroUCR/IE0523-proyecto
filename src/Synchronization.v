bc3366f97226b43af1cb411107082077c17b649e
DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>
2024-11-13 17:13:38 -0600

    Modificacion de módulo Synchronization. Se modifica la lógica y estructura del módulo. Cambios en las salidas, uso de flip-flops para manejo de lógica, etc.
    Signed-off-by: DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>

--- src/Synchronization.v
+++ src/Synchronization.v
@@ -11,34 +11,62 @@
  * MIT License
  */
 
+/* ++++++++++++++++++++ Definiciones constantes ++++++++++++++++++++ */
+    `define TRUE    1'b1
+    `define FALSE   1'b0
+    `define OK      1'b1
+    `define FAIL    1'b0
 
-// ++++++++++++++++++++ Módulo Synchronization ++++++++++++++++++++
-module Synchronization(input Clk, Reset,                // Señales comunes de chip
-                       input [9:0] rx_code_group,       // Code group recibido del PMA, REVISAR SI DEBERÍA SER rx_code_group o si es PUDI en sí
-                       input PUDI,                      // Indicación PUDI.. expandir
-                       output reg code_synch_status,    // Estatus de sincronización
-                       output reg rx_even,              // Indicador de paridad del codegroup
-                       output reg [9:0] PUDI);          // Señal PUDI de salida
+/* ++++++++++++++++++++ Módulo Synchronization +++++++++++++++++++++ */
+module Synchronization(
+    input Clk,                      // Señal de reloj
+    input mr_main_reset,            // Señal de reset
+    input power_on,                 // Señal de encencido
+    input [9:0] PUDI,               // Code group recibido del PMA
+    output reg code_sync_status,    // Estatus de sincronización
+    output reg [11:0] SUDI);        // Señal SUDI de salida, contiene el code group y rx_even
+
+    // output reg rx_even, removido para incluirlo en SUDI
+    //input PUDI REVISAR SI DEBERÍA SER rx_code_group o si es PUDI en sí
 
 /* ++++++++++++++++++++++++ Asignación de registros internos ++++++++++++++++++++++++ */
     // Vectores de estado presente y de próximo estado
-    reg [6:0] EstPres, ProxEstado;
+    reg [6:0] State, next_State;
+
+    // rx_even
+    reg rx_even, next_rx_even;
+
+    // code_sync_status
+    reg next_code_sync_status;
+
+    // Manejo de PUDI
+    reg PUDI_INVALID, PUDI_COMMA, PUDI_D;
 
     // Manejo de códigos buenos
-    reg [1:0] good_cgs, next_good_cgs;
+    reg [1:0] good_cgs, next_good_cgs; // Contadores
 
     wire cggood;
-    assign cggood = (!(INVALID | COMMA & rx_even) & PUDI);
+    assign cggood = !(PUDI_INVALID | PUDI_COMMA & rx_even);
 
-    wire INVALID;
-    assign INVALID = 1'b1;  // To do: Verificar que el código es parte de los válidos.
+/* ++++++++++++++++++++++++++++ Determinación de PUDI ++++++++++++++++++++++++++++++ */
+always @(*) begin
+    case (PUDI)     // COMMA
+        10'b0000000000: PUDI_COMMA = `TRUE;
+        default: PUDI_COMMA = `FALSE;
+    endcase
 
-    wire COMMA;
-    assign COMMA = 1'b1;    // To do: Verificar que el código es una comma, ver pag 63.
+    case (PUDI)     // /D/
+        10'b0000000000: PUDI_D = `TRUE;
+        default: PUDI_D = `FALSE;
+    endcase
 
-/* +++++++++++++++++++++++++++++ Asignación de estados +++++++++++++++++++++++++++++ */
-   // Se asignan los estados según el diagrama ASM de la máquina.
-   // Se usa un FF para cada estado para evitar race conditions durante operacion del IC.
+    case (PUDI)     // /INVALID/
+        10'b0000000000: PUDI_INVALID = `TRUE;
+        default: PUDI_INVALID = `FALSE;
+    endcase
+end
+
+/* +++++++++++++++++++++++++++++ Asignación de estados ++++++++++++++++++++++++++++++ */
     localparam [8:0]
         LOSS_OF_SYNC     = 9'b000000001,
         COMMA_DETECT_1   = 9'b000000010,
@@ -50,26 +78,35 @@ module Synchronization(input Clk, Reset,                // Señales comunes de c
         SYNC_ACQUIRED_2  = 9'b010000000,
         SYNC_ACQUIRED_2A = 9'b100000000;
 
-
-/* +++++++++++++++++++++++++++++ Definición de Flip Flops +++++++++++++++++++++++++++++ */
-    // Se define el flanco creciente de reloj como el flanco activo de reloj y reset sincrónico.
-    always @(posedge Clk, negedge Reset)
-        if (~Reset)  begin
-            EstPres    <= Estado_Idle;
+/* +++++++++++++++++++++++++++++ Definición de Flip Flops +++++++++++++++++++++++++++ */
+    always @(posedge Clk) begin
+        if (mr_main_reset || mr_main_reset)  begin
+            State            <= LOSS_OF_SYNC;
+            good_cgs         <= 2'b0;
+            rx_even          <= `FALSE;     // Se define para que no sea X, pero es irrelevante.
+            code_sync_status <= `FAIL;
         end
         else begin
-            EstPres    <= ProxEstado;
+            State            <= next_State;
+            good_cgs         <= next_good_cgs;
+            rx_even          <= next_rx_even;
+            code_sync_status <= next_code_sync_status;
         end
+        SUDI <= {PUDI, rx_even};            // Se declara la salida SUDI
+    end
 
-/* +++++++++++++++++++++++++++++ Lógica combinacional +++++++++++++++++++++++++++++ */
+/* +++++++++++++++++++++++++++++ Cambio de estados ++++++++++++++++++++++++++++++++++ */
     always @(*) begin
 
     // Valores por defecto para mantener el comportamiento de FF.
-    ProxEstado = EstPres;
+    next_State            = State;
+    next_good_cgs         = good_cgs;
+    next_rx_even          = rx_even;
+    next_code_sync_status = code_sync_status;
 
     // Lógica combinacional según el estado presente.
-    case (EstPres)
-        default: ProxEstado = Estado_Idle;
+    case (State)
+        default: next_State = LOSS_OF_SYNC;
     endcase
     end
 
