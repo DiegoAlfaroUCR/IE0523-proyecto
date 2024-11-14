297f239f7a879696db18e60e783b626735ba4cdf
Bryan Cortes <bryanco0310@gmail.com>
2024-11-13 22:04:10 -0600

    Merge de los branch
    Signed-off-by: Bryan Cortes <bryanco0310@gmail.com>

--- Receive/Receive.v
+++ Receive/Receive.v
@@ -1,4 +1,4 @@
-`include "code_grups.v"
+`include "tablas.v"
 
 module Receive (
     input clk,
@@ -20,34 +20,6 @@ module Receive (
 
     reg [7:0] current_state, next_state;
     reg receiving;
-
-function [9:0] D;
-   input [9:0] SUDI; 
-   case(SUDI)
-    b10_D0_0:  D = b10_D0_0;    
-    b10_D1_0:  D = b10_D1_0;   
-    b10_D2_0:  D = b10_D2_0;  
-    b10_D3_0:  D = b10_D3_0;  
-    b10_D4_0:  D = b10_D4_0;  
-    b10_D5_0:  D = b10_D5_0;    
-    b10_D6_0:  D = b10_D6_0;   
-    b10_D7_0:  D = b10_D7_0;  
-    b10_D8_0:  D = b10_D8_0;      
-    b10_D9_0:  D = b10_D9_0;  
-    b10_K28_0: D = b10_K28_0; 
-    b10_K28_1: D = b10_K28_1; 
-    b10_K28_2: D = b10_K28_2; 
-    b10_K28_3: D = b10_K28_3; 
-    b10_K28_4: D = b10_K28_4; 
-    b10_K28_5: D = b10_K28_5; 
-    b10_K28_6: D = b10_K28_6;      
-    b10_K28_7: D = b10_K28_7;
-    b10_K23_7: D = b10_K23_7;
-    b10_K27_7: D = b10_K27_7;
-    b10_K29_7: D = b10_K29_7; 
-    b10_K30_7: D = b10_K30_7;
-   endcase
-endfunction
 
 
 always @(posedge clk ) begin
